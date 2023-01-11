2387281cfb32cdd02156a0a365126b282d69e861
Ianphantom <ian25yola@gmail.com>
2023-01-11 23:09:57 +0700

    Accordion Component

---
+++ src/components/DetailAccordion.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+
+const DetailAccordion = ({ title, desc, link }) => {
+  const [accordionStatus, setAccordionStatus] = useState(false);
+  return (
+    <AccordionContainer onClick={() => setAccordionStatus(!accordionStatus)}>
+      <div className='headerAccordion'>
+        <div className='myProject'>{title}</div>
+        <svg
+          style={{
+            transform: `rotate(${accordionStatus == true ? "45deg" : "0"})`,
+          }}
+          width='24'
+          height='25'
+          viewBox='0 0 24 25'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <g clipPath='url(#clip0_227_1168)'>
+            <path
+              d='M3.75 12.2969H20.25'
+              stroke='#222222'
+              strokeWidth='2.25'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+            <path
+              d='M12 4.04688V20.5469'
+              stroke='#222222'
+              strokeWidth='2.25'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+          </g>
+          <defs>
+            <clipPath id='clip0_227_1168'>
+              <rect
+                width='24'
+                height='24'
+                fill='white'
+                transform='translate(0 0.296875)'
+              />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+      {accordionStatus && (
+        <>
+          <div className='descAccordion'>{desc}</div>
+          <div className='descAccordion learnMore'>Learn More</div>
+        </>
+      )}
+    </AccordionContainer>
+  );
+};
+
+const AccordionContainer = styled.div`
+  cursor: pointer;
+  padding: 24px 24px;
+  background: #fafafa;
+  border-radius: 20px;
+  margin-bottom: 4px;
+  .headerAccordion {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .myProject {
+      /* font-family: "Inter"; */
+      /* font-family: "Space Grotesk"; */
+      font-family: "GT Walsheim Medium", serif;
+
+      font-style: normal;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 26px;
+      letter-spacing: -0.5px;
+      color: #222222;
+    }
+  }
+  .descAccordion {
+    margin-top: 30px;
+    /* font-family: "Inter"; */
+    /* font-family: "Space Grotesk"; */
+    font-family: "GT Walsheim Medium", serif;
+
+    font-style: normal;
+    font-size: 18px;
+    line-height: 26px;
+    letter-spacing: -0.5px;
+    font-weight: 400;
+    color: #818181;
+  }
+
+  .learnMore {
+    color: #0088ff;
+  }
+`;
+
+export default DetailAccordion;
--- src/pages/ReactDetail.js
+++ src/pages/ReactDetail.js
@@ -1,8 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 // component
 import DetailHeadComponent from "../components/DetailHeadComponent";
 import DetailSlideShow from "../components/DetailSlideShow";
+import DetailAccordion from "../components/DetailAccordion";
 
 const ReactDetail = () => {
   const images = [
@@ -11,7 +13,7 @@ const ReactDetail = () => {
     "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png",
   ];
   return (
-    <div>
+    <DetailContainer>
       <div className='container'>
         <DetailHeadComponent
           title={"Template for SaaS Companies"}
@@ -24,12 +26,74 @@ const ReactDetail = () => {
       </div>
 
       <DetailSlideShow images={images} />
+      <div className='container'>
+        <div className='detail'>
+          <div className='left'>
+            <div className='title'>My Project</div>
+            <div className='accordionContainer'>
+              <DetailAccordion
+                title='Ignite Website'
+                desc={`There are two types of scroll effects: Speed and Appear. Speed allows you to define the scrolling speed of a layer, which allows to you to design parallax effects. Appear allows you to animate any layer as you scroll past it (as it enters the viewport). The Appear effect allows you to animate layers as you scroll past them. See a short summary of the available properties below.`}
+                link={"/detail"}
+              />
+              <DetailAccordion
+                title='Ignite Website'
+                desc={`There are two types of scroll effects: Speed and Appear. Speed allows you to define the scrolling speed of a layer, which allows to you to design parallax effects. Appear allows you to animate any layer as you scroll past it (as it enters the viewport). The Appear effect allows you to animate layers as you scroll past them. See a short summary of the available properties below.`}
+                link={"/detail"}
+              />
+              <DetailAccordion
+                title='Ignite Website'
+                desc={`There are two types of scroll effects: Speed and Appear. Speed allows you to define the scrolling speed of a layer, which allows to you to design parallax effects. Appear allows you to animate any layer as you scroll past it (as it enters the viewport). The Appear effect allows you to animate layers as you scroll past them. See a short summary of the available properties below.`}
+                link={"/detail"}
+              />
+              <DetailAccordion
+                title='Ignite Website'
+                desc={`There are two types of scroll effects: Speed and Appear. Speed allows you to define the scrolling speed of a layer, which allows to you to design parallax effects. Appear allows you to animate any layer as you scroll past it (as it enters the viewport). The Appear effect allows you to animate layers as you scroll past them. See a short summary of the available properties below.`}
+                link={"/detail"}
+              />
+            </div>
+          </div>
+          <div className='right'>
+            <div>Pages</div>
+            <div>Features</div>
+          </div>
+        </div>
+      </div>
       <div className='componentAccordion'></div>
       <div className='componentPages'></div>
       <div className='componenSupport'></div>
       <div className='componentCategories'></div>
-    </div>
+    </DetailContainer>
   );
 };
+
+const DetailContainer = styled.div`
+  .detail {
+    display: flex;
+    justify-content: space-between;
+    gap: 80px;
+    padding: 80px 0px;
+    .title {
+      margin-bottom: 41px;
+      /* font-family: "Inter"; */
+      font-family: "GT Walsheim Regular", serif;
+
+      font-style: normal;
+      font-weight: 600;
+      font-size: 40px;
+      line-height: 48px;
+      letter-spacing: -1.2px;
+    }
+    .left {
+      width: 100%;
+    }
+  }
+  @media (max-width: 768px) {
+    .detail {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+`;
 
 export default ReactDetail;
