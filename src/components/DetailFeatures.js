3ca00b16bbf3892b6ded9cb77847ddcb224b9763
tugasSBD <ian25yola@gmail.com>
2023-02-01 13:19:11 +0700

    Perbaikan detail features menggunakan prop

--- src/components/DetailFeatures.js
+++ src/components/DetailFeatures.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const DetailFeatures = () => {
+const DetailFeatures = ({ desc }) => {
   return (
     <FeaturesContainer>
       <svg
@@ -14,47 +14,47 @@ const DetailFeatures = () => {
         <path
           d='M6 12.0469C8.8995 12.0469 11.25 9.69637 11.25 6.79688C11.25 3.89738 8.8995 1.54688 6 1.54688C3.10051 1.54688 0.75 3.89738 0.75 6.79688C0.75 9.69637 3.10051 12.0469 6 12.0469Z'
           stroke='#0088FF'
-          stroke-width='1.3125'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.3125'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path
           d='M6 8.98438C7.20812 8.98438 8.1875 8.005 8.1875 6.79688C8.1875 5.58875 7.20812 4.60938 6 4.60938C4.79188 4.60938 3.8125 5.58875 3.8125 6.79688C3.8125 8.005 4.79188 8.98438 6 8.98438Z'
           stroke='#0088FF'
-          stroke-width='1.3125'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.3125'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path
           d='M4.45225 5.24924L2.28662 3.08362'
           stroke='#0088FF'
-          stroke-width='1.3125'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.3125'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path
           d='M7.54761 5.24924L9.71323 3.08362'
           stroke='#0088FF'
-          stroke-width='1.3125'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.3125'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path
           d='M7.54761 8.34448L9.71323 10.5101'
           stroke='#0088FF'
-          stroke-width='1.3125'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.3125'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path
           d='M4.45225 8.34448L2.28662 10.5101'
           stroke='#0088FF'
-          stroke-width='1.3125'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.3125'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </svg>
-      <div className='desc'>How templates work</div>
+      <div className='desc'>{desc}</div>
     </FeaturesContainer>
   );
 };
