d23af26e50a353640b00e20964ae3946b259311d
tugasSBD <ian25yola@gmail.com>
2023-02-01 13:34:17 +0700

    Perbaikan Detail Pages

--- src/components/DetailPages.js
+++ src/components/DetailPages.js
@@ -11,18 +11,18 @@ const DetailPages = () => {
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
       >
-        <g clip-path='url(#clip0_227_1214)'>
+        <g clipPath='url(#clip0_227_1214)'>
           <path
             d='M6 0.546875C9.314 0.546875 12 3.23288 12 6.54688C12 9.86087 9.314 12.5469 6 12.5469C2.686 12.5469 0 9.86087 0 6.54688C0 3.23288 2.686 0.546875 6 0.546875Z'
             stroke='#999999'
-            stroke-width='3'
+            strokeWidth='3'
           />
           <path
             d='M6 0.546875C7.657 0.546875 9 3.23288 9 6.54688C9 9.86087 7.657 12.5469 6 12.5469C4.343 12.5469 3 9.86087 3 6.54688C3 3.23288 4.343 0.546875 6 0.546875Z'
             stroke='#999999'
-            stroke-width='3'
+            strokeWidth='3'
           />
-          <path d='M1 6.54688H11' stroke='#999999' stroke-width='1.5' />
+          <path d='M1 6.54688H11' stroke='#999999' strokeWidth='1.5' />
         </g>
         <defs>
           <clipPath id='clip0_227_1214'>
