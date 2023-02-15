5b9ecbc24250a1d39a79122dc94ff5b7b3740222
tugasSBD <ian25yola@gmail.com>
2023-02-16 00:40:26 +0700

    Project State

--- src/ProjectState.js
+++ src/ProjectState.js
@@ -1,22 +1,58 @@
-import realty from "./images/realty.png";
-
 export const ProjectState = [
   {
     index: 1,
     number: "01",
-    image: realty,
-    text: "1234323e",
+    image:
+      "https://plus.unsplash.com/premium_photo-1668418188837-d40b734ed6d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    text: "React Music Player",
   },
   {
     index: 2,
-    number: "01",
-    image: realty,
-    text: "1234323e",
+    number: "02",
+    image:
+      "https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=647&q=80",
+    text: "Ignite Catalog",
   },
   {
     index: 3,
-    number: "01",
-    image: realty,
-    text: "1234323e",
+    number: "03",
+    image:
+      "https://images.unsplash.com/photo-1525909002-1b05e0c869d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    text: "Color Picker",
+  },
+  {
+    index: 4,
+    number: "04",
+    image:
+      "https://images.unsplash.com/photo-1571901206014-d1ea0451855e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    text: "Beat Maker",
+  },
+  {
+    index: 5,
+    number: "05",
+    image:
+      "https://images.unsplash.com/photo-1643197368807-79ba40157302?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=678&q=80",
+    text: "Photon Project",
+  },
+  {
+    index: 6,
+    number: "06",
+    image:
+      "https://images.unsplash.com/photo-1505118380757-91f5f5632de0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80",
+    text: "Travel Project",
+  },
+  {
+    index: 7,
+    number: "07",
+    image:
+      "https://images.unsplash.com/photo-1532567621246-f0432eab0522?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    text: "Capture Project",
+  },
+  {
+    index: 8,
+    number: "08",
+    image:
+      "https://images.unsplash.com/photo-1485968579580-b6d095142e6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
+    text: "Fashion Project",
   },
 ];
