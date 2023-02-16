618914b91b7ecf874ffdbf61f5e0b6a2d1ea1221
tugasSBD <ian25yola@gmail.com>
2023-02-16 16:38:57 +0700

    Project State Update

--- src/ProjectState.js
+++ src/ProjectState.js
@@ -5,6 +5,7 @@ export const ProjectState = [
     image:
       "https://plus.unsplash.com/premium_photo-1668418188837-d40b734ed6d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     text: "React Music Player",
+    link: "https://effulgent-cupcake-e5436d.netlify.app/",
   },
   {
     index: 2,
@@ -12,6 +13,7 @@ export const ProjectState = [
     image:
       "https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=647&q=80",
     text: "Ignite Catalog",
+    link: "https://zingy-yeot-7ffc95.netlify.app/",
   },
   {
     index: 3,
@@ -19,6 +21,7 @@ export const ProjectState = [
     image:
       "https://images.unsplash.com/photo-1525909002-1b05e0c869d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
     text: "Color Picker",
+    link: "https://eloquent-raindrop-17a6fa.netlify.app/",
   },
   {
     index: 4,
@@ -26,6 +29,7 @@ export const ProjectState = [
     image:
       "https://images.unsplash.com/photo-1571901206014-d1ea0451855e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     text: "Beat Maker",
+    link: "https://clinquant-elf-8b4ca9.netlify.app/",
   },
   {
     index: 5,
@@ -33,6 +37,7 @@ export const ProjectState = [
     image:
       "https://images.unsplash.com/photo-1643197368807-79ba40157302?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=678&q=80",
     text: "Photon Project",
+    link: "https://guileless-concha-f8ad52.netlify.app/",
   },
   {
     index: 6,
@@ -40,6 +45,7 @@ export const ProjectState = [
     image:
       "https://images.unsplash.com/photo-1505118380757-91f5f5632de0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80",
     text: "Travel Project",
+    link: "https://endearing-sprite-2efae5.netlify.app/",
   },
   {
     index: 7,
@@ -47,12 +53,14 @@ export const ProjectState = [
     image:
       "https://images.unsplash.com/photo-1532567621246-f0432eab0522?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
     text: "Capture Project",
+    link: "https://exquisite-zabaione-83486d.netlify.app/",
   },
   {
     index: 8,
     number: "08",
     image:
       "https://images.unsplash.com/photo-1485968579580-b6d095142e6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
-    text: "Fashion Project",
+    text: "Photography Project",
+    link: "https://admirable-churros-ec27ff.netlify.app/",
   },
 ];
