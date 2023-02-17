caf0167ce6a0150d3768a604e0a864e2cfc9bfce
tugasSBD <ian25yola@gmail.com>
2023-02-17 16:31:16 +0700

    Add category at projectState Data

--- src/ProjectState.js
+++ src/ProjectState.js
@@ -2,6 +2,7 @@ export const ProjectState = [
   {
     index: 1,
     number: "01",
+    category: "React",
     image:
       "https://plus.unsplash.com/premium_photo-1668418188837-d40b734ed6d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     text: "React Music Player",
@@ -10,6 +11,7 @@ export const ProjectState = [
   {
     index: 2,
     number: "02",
+    category: "React",
     image:
       "https://images.unsplash.com/photo-1509198397868-475647b2a1e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=647&q=80",
     text: "Ignite Catalog",
@@ -18,6 +20,7 @@ export const ProjectState = [
   {
     index: 3,
     number: "03",
+    category: "Javascript",
     image:
       "https://images.unsplash.com/photo-1525909002-1b05e0c869d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
     text: "Color Picker",
@@ -26,6 +29,7 @@ export const ProjectState = [
   {
     index: 4,
     number: "04",
+    category: "Javascript",
     image:
       "https://images.unsplash.com/photo-1571901206014-d1ea0451855e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     text: "Beat Maker",
@@ -34,6 +38,7 @@ export const ProjectState = [
   {
     index: 5,
     number: "05",
+    category: "Javascript",
     image:
       "https://images.unsplash.com/photo-1643197368807-79ba40157302?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=678&q=80",
     text: "Photon Project",
@@ -42,6 +47,7 @@ export const ProjectState = [
   {
     index: 6,
     number: "06",
+    category: "Javascript",
     image:
       "https://images.unsplash.com/photo-1505118380757-91f5f5632de0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80",
     text: "Travel Project",
@@ -50,6 +56,7 @@ export const ProjectState = [
   {
     index: 7,
     number: "07",
+    category: "React",
     image:
       "https://images.unsplash.com/photo-1532567621246-f0432eab0522?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
     text: "Capture Project",
@@ -58,6 +65,7 @@ export const ProjectState = [
   {
     index: 8,
     number: "08",
+    category: "Others",
     image:
       "https://images.unsplash.com/photo-1485968579580-b6d095142e6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
     text: "Photography Project",
